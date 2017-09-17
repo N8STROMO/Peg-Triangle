84183bea284c4f29290bb57955a3f302075c75dc
NO8STROMO <johntpaul93@gmail.com>
2017-09-16 21:09:42 -0400

    Progress

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+  private Vector2 mouseOver;
+  private Vector2 dragStart;
+  private Vector2 dragEnd;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,4 +17,24 @@ public class Movement : MonoBehaviour {
 	void Update () {
 		
 	}
+
+  void UpdateMouseOver()
+  {
+
+  }
+
+  void SelectPiece()
+  {
+
+  }
+
+  void UpdatePieceDrag()
+  {
+
+  }
+
+  void MovePiece()
+  {
+
+  }
 }
--- Assets/Scripts/PegTriangle.cs
+++ Assets/Scripts/PegTriangle.cs
@@ -1,6 +1,6 @@
 ﻿// TODO Moving pegs within triangle
 
-using UnityEngine;
+ using UnityEngine;
 
 public class Space
 {
@@ -24,13 +24,31 @@ public class PegTriangle : MonoBehaviour {
   public float desiredMovedDistance;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+  {
     GenerateBoard();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+  { 
+    if (turnCount == 0)
+    {
+      if (Input.GetMouseButtonDown(0))
+      {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        for (int i = 0; i < board.Length; i++)
+        {
+          if (Vector2.Distance(board[i].associatedObject.transform.position, mousePosition) <= .3f) 
+          {
+            board[i].filled = false;
+            board[i].associatedObject.SetActive(false);
+            turnCount++;
+            break;
+          }
+        }
+      }
+    }
 	}
 
   void GenerateBoard()
@@ -47,7 +65,8 @@ public class PegTriangle : MonoBehaviour {
     {
       board[i] = new Space();
       board[i].filled = true;
-      GameObject peg = Instantiate(pegPrefab, GetPegWorldPosition(i), Quaternion.identity, transform);
+      GameObject peg = Instantiate(pegPrefab, transform, false);
+      peg.transform.localPosition = GetPegWorldPosition(i);
       board[i].associatedObject = peg;
       board[i].worldPosition = GetPegWorldPosition(i);
     }
